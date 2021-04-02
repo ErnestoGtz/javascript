7b1ed154f2119adfc38778db07cb5b457f03d09e
Ernesto <ernesto.gtz@itcoach.com.mx>
2021-04-02 08:13:19 -0600

    params

---
+++ js/18-funcionesParametros.js
@@ -0,0 +1,14 @@
+//Declaracion de funcion
+function sumar(numero1 = 0,numero2 = 0){
+    console.log(numero1 + numero2);    
+}
+
+sumar(20,50);
+
+// Expresion de la funcion
+
+const sumar2 = function(n1,n2){
+    console.log(n1 + n2);
+}
+
+sumar2(45,78);
