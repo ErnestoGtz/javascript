691a029eb7bcc5376021b8ca442484dabe23eee9
Ernesto <ernesto.gtz@itcoach.com.mx>
2021-04-06 09:03:15 -0500

    learning javascript continue

--- js/24-for.js
+++ js/24-for.js
@@ -1,6 +1,49 @@
 // for loop
 
-for(i = 0; i < 10; i++){
-    console.log(`Instruccion ${i}`);
-}
+// for(let i = 0; i <= 10; i++){
+//     console.log(`Instruccion ${i}`);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     if( i % 2 == 0){
+//         console.log(`el numero ${i} es un numero par`);
+//     } else 
+//         console.log(`el numero ${i} es un numero impar`);
+// }
+
+
+// const carrito = [
+//     {
+//         nombre:'Monitor 20 Pulgadas',
+//         precio:2000
+//     },
+//     {
+//         nombre:'Audifonos Gamer',
+//         precio:1000
+//     },
+//     {
+//         nombre:'Laptop Gamer MSI',
+//         precio:25000
+//     },
+//     {
+//         nombre:'Teclado Mecanico',
+//         precio:1250
+//     },
+//     {
+//         nombre:'Iphone XR',
+//         precio:10000
+//     },
+//     {
+//         nombre:'Bocinas Logitech',
+//         precio:550
+//     }
+// ];
+
+// for (let i = 0; i < carrito.length; i++) {
+//     console.log(carrito[i].nombre);
+// }
+
+// While loop
+
+let i = 0;
 
